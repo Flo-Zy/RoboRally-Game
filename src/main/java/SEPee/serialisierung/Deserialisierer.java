6624337b682a4dd0dc1742c97ce63b55ebc43956
Maximilian Fink <Fink.Maximilian@campus.lmu.de>
2023-12-07 12:13:22 +0100

    updated Deserialisierer Serialisierer [MF]

--- src/main/java/SEPee/serialisierung/Deserialisierer.java
+++ src/main/java/SEPee/serialisierung/Deserialisierer.java
@@ -1,8 +1,11 @@
 package SEPee.serialisierung;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Deserialisierer {
-    private static final Gson gson = new Gson();
+    private static final Gson gson = new GsonBuilder()
+            .registerTypeAdapterFactory(new FieldTypeAdapterFactory())
+            .create();
 
     public static <T> T deserialize(String json, Class<T> clazz) {
         return gson.fromJson(json, clazz);
