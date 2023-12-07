d944e3d768599a4fc793a05c76d83c537661d7de
Maximilian Fink <Fink.Maximilian@campus.lmu.de>
2023-12-07 12:06:09 +0100

    updated StartPoint.java [MF]

--- src/main/java/SEPee/server/model/field/StartPoint.java
+++ src/main/java/SEPee/server/model/field/StartPoint.java
@@ -1,13 +1,19 @@
 package SEPee.server.model.field;
-import SEPee.server.model.Position;
+
+import com.google.gson.annotations.SerializedName;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class StartPoint extends Field{
+    //private int speed;
+    //private String[] orientations;
+    //private int[] registers;
+    //private int count;
 
-    public StartPoint(Position position){
-        super("Startpoint", position);
+    public StartPoint(String isOnBoard){
+        super("StartPoint", isOnBoard);
     }
 }
