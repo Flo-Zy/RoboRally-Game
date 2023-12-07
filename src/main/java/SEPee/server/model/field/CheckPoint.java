40f2f153902580becc6671ad9f41223a1219e436
Maximilian Fink <Fink.Maximilian@campus.lmu.de>
2023-12-07 11:58:43 +0100

    updated CheckPoint.java[MF]

--- src/main/java/SEPee/server/model/field/CheckPoint.java
+++ src/main/java/SEPee/server/model/field/CheckPoint.java
@@ -1,13 +1,16 @@
 package SEPee.server.model.field;
-import SEPee.server.model.Position;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CheckPoint extends Field{
-    private int counter;
-    public CheckPoint(Position position){
-        super("Checkpoint", position);
+    //private int speed;
+    //private String[] orientations;
+    //private int[] registers;
+    //private int count;
+    public CheckPoint(String isOnBoard){
+        super("CheckPoint", isOnBoard);
     }
 }
